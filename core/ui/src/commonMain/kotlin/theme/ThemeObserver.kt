e1eb6d9773ca33ce9948c42489c344801941f909
Melika Afrakhteh <melikaafrakhteh@gmail.com>
2025-06-28 16:53:22 +0330

    remove core ui module

--- core/ui/src/commonMain/kotlin/theme/ThemeObserver.kt
+++
@@ -1,5 +0,0 @@
-package theme
-
-expect class ThemeObserver() {
-    fun startObservingThemeChanges(onThemeChanged: (Boolean) -> Unit)
-}
--- core/ui/src/jsMain/kotlin/theme/ThemeObserver.js.kt
+++
@@ -1,6 +0,0 @@
-package theme
-
-actual class ThemeObserver {
-    actual fun startObservingThemeChanges(onThemeChanged: (Boolean) -> Unit) {
-    }
-}
